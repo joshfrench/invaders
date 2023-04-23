88050390f6792388f3ba6a90c1482a167bf7b8ec
Josh French <joshfrench@gmail.com>
2023-04-23 12:44:56 -0400

    enemies

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,36 @@ fn setup(
 
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        SpriteSheetBundle {
+    commands
+        .spawn(SpriteSheetBundle {
             texture_atlas: texture_atlas_handle.clone(),
             transform: Transform::from_translation(Vec3::new(0.0, -220.0, 0.0)),
             sprite: TextureAtlasSprite::new(0),
             ..default()
-        },
-        Player::new(),
-    ));
+        })
+        .insert(Player::new());
+
+    for bug_row in 0..4 {
+        let y = 200.0 - (bug_row as f32 * 30.0);
+        for bug_col in 0..20 {
+            let x = -300.0 + (bug_col as f32 * 30.0);
+            commands
+                .spawn_empty()
+                .insert(SpriteSheetBundle {
+                    texture_atlas: texture_atlas_handle.clone(),
+                    transform: Transform::from_translation(Vec3::new(x, y, 0.0)),
+                    sprite: TextureAtlasSprite::new(1),
+                    ..default()
+                })
+                .insert(Bug {
+                    movement: if bug_row % 2 == 0 {
+                        BugMovement::Left
+                    } else {
+                        BugMovement::Right
+                    },
+                });
+        }
+    }
 }
 
 #[derive(Component)]
@@ -46,17 +67,14 @@ fn player(
         let mut firing = false;
 
         if keyboard_input.pressed(KeyCode::Left) {
-            eprintln!("Left!");
             player.delta_x -= ACCELERATION;
         }
 
         if keyboard_input.pressed(KeyCode::Right) {
-            eprintln!("Right!");
             player.delta_x += ACCELERATION;
         }
 
         if keyboard_input.just_pressed(KeyCode::Space) {
-            eprintln!("Fire!");
             firing = true;
         }
 
@@ -66,6 +84,59 @@ fn player(
 
         // Decelerate
         player.delta_x *= 0.75;
+    }
+}
+
+#[derive(Copy, Clone)]
+enum BugMovement {
+    Left,
+    Right,
+    Down { n: f32, next_left: bool },
+}
+
+#[derive(Component)]
+struct Bug {
+    movement: BugMovement,
+}
+
+fn bug_movement(mut query: Query<(&mut Bug, &mut Transform)>) {
+    for (mut bug, mut trans) in query.iter_mut() {
+        let mut new_movement = bug.movement;
+        match bug.movement {
+            BugMovement::Left => {
+                trans.translation.x -= 2.0;
+                if trans.translation.x < -300.0 {
+                    new_movement = BugMovement::Down {
+                        n: 12.0,
+                        next_left: false,
+                    };
+                }
+            }
+            BugMovement::Right => {
+                trans.translation.x += 2.0;
+                if trans.translation.x > 300.0 {
+                    new_movement = BugMovement::Down {
+                        n: 12.0,
+                        next_left: true,
+                    };
+                }
+            }
+            BugMovement::Down { n, next_left } => {
+                trans.translation.y -= 2.0;
+                new_movement = BugMovement::Down {
+                    n: n - 1.0,
+                    next_left,
+                };
+                if n < 1.0 {
+                    new_movement = if next_left {
+                        BugMovement::Left
+                    } else {
+                        BugMovement::Right
+                    };
+                }
+            }
+        }
+        bug.movement = new_movement;
     }
 }
 
@@ -82,5 +153,6 @@ fn main() {
         }))
         .add_startup_system(setup)
         .add_system(player)
+        .add_system(bug_movement)
         .run();
 }
